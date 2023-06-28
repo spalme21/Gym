ac2fa2c70376ca4eeb4b91c0744c7de9c023183b
spalme21 <84144056+spalme21@users.noreply.github.com>
2023-06-28 19:55:00 +0000

    Add methods to add clients and trainers

--- Gym.java
+++ Gym.java
@@ -3,11 +3,6 @@ import java.util.ArrayList;
 public class Gym {
 
     /**
-     * The name of the gym.
-     */
-    private String name;
-
-    /**
      * A list of the gym's clients.
      */
     private ArrayList<Client> clients;
@@ -19,15 +14,41 @@ public class Gym {
 
     /**
      * Create a new gym
-     * @param name  The name of the gym
      */
-    public Gym(String name) {
-        
-        this.name = name;
+    public Gym() {
 
         // start with no clients or trainers
         this.clients = new ArrayList<Client>();
         this.trainers = new ArrayList<Trainer>();
 
+    }
+
+
+    /**
+     * Create a new client
+     * @param lastName      the client's last name
+     * @param firstName     the client's first name
+     * @param phoneNumber   the client's phone number
+     * @param email         the client's email
+     * @return              the new Client object
+     */
+    public Client addClient(String lastName, String firstName, String phoneNumber, String email) {
+
+        // create a new client and add it to the list of clients
+        Client newClient = new Client(lastName, firstName, phoneNumber, email);
+        this.clients.add(newClient);
+
+        return newClient;
+
+    }
+
+    public Trainer addTrainer(String lastName, String firstName) {
+        
+        // create a new trainer and add it to the list of trainers
+        Trainer newTrainer = new Trainer(lastName, firstName);
+        this.trainers.add(newTrainer);
+
+        return newTrainer;
+        
     }
 }
