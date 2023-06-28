a608755e225625adcadd7a59b068a2c8ea00cd8e
spalme21 <84144056+spalme21@users.noreply.github.com>
2023-06-28 17:46:05 +0000

    Add credits and session history to Client.

--- Client.java
+++ Client.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Client {
 
     /**
@@ -21,6 +23,16 @@ public class Client {
     private String email;
 
     /**
+     * The credit balance of the client.
+     */
+    private int creditBalance;
+
+    /**
+     * The list of sessions for the client.
+     */
+    private ArrayList<Session> sessionHistory;
+
+    /**
      * Create a new client
      * @param lastName      the client's last name
      * @param firstName     the client's first name
@@ -29,10 +41,15 @@ public class Client {
      */
     public Client(String lastName, String firstName, String phoneNumber, String email) {
 
+        // set client's name and contact info
         this.lastName = lastName;
         this.firstName = firstName;
         this.phoneNumber = phoneNumber;
         this.email = email;
+
+        // start client with 0 credits and 0 sessions
+        this.credits = 0;
+        this.sessionHistory = new ArrayList<Session>();
 
         // print log message
         System.out.printf("New client %s, %s created.", this.firstName, this.lastName);
